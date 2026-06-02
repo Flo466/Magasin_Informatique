ba9581c7762e67bea71e11ec544f4c5f5c949d5e
Yanis Aitlamine <yanis.aitlamine@gmail.com>
2026-06-02 13:23:45 +0000

    fix arborescence pardon j'avais tout cassé

--- sql/creation_base.sql
+++ sql/creation_base.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE magasin_informatique;
 USE magasin_informatique;
-
 CREATE TABLE clients (
     id INT AUTO_INCREMENT,
     nom VARCHAR(50) NOT NULL,
@@ -8,14 +7,13 @@ CREATE TABLE clients (
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
     adresse VARCHAR(150),
-    telephone VARCHAR(20), -- This comma is correct because another item follows
-    PRIMARY KEY (id)       -- <-- NO COMMA HERE!
+    telephone VARCHAR(20),
+    PRIMARY KEY (id)
 );
-
 CREATE TABLE produits (
-	id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     nom VARCHAR(100) NOT NULL,
-    desciption TEXT,
+    description TEXT,
     prix DECIMAL(10,2) NOT NULL DEFAULT 0,
     stock INT NOT NULL DEFAULT 0,
     image VARCHAR(100),
@@ -23,8 +21,6 @@ CREATE TABLE produits (
     CHECK (prix >= 0),
     CHECK (stock >= 0)
 );
-
--- 4) Table : commandes
 CREATE TABLE commandes (
     id INT AUTO_INCREMENT,
     id_client INT NOT NULL,
@@ -33,10 +29,7 @@ CREATE TABLE commandes (
     statut VARCHAR(30) DEFAULT 'En attente',
     PRIMARY KEY (id),
     FOREIGN KEY (id_client) REFERENCES clients(id)
-    ON DELETE CASCADE
 );
-
--- 5) Table : details_commandes
 CREATE TABLE details_commandes (
     id INT AUTO_INCREMENT,
     id_commande INT NOT NULL,
@@ -44,10 +37,8 @@ CREATE TABLE details_commandes (
     quantite INT NOT NULL,
     prix_unitaire DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_commande) REFERENCES commandes(id)
-    ON DELETE CASCADE,
-    FOREIGN KEY (id_produit) REFERENCES produits(id)
-    ON DELETE CASCADE,
-    CHECK (quantite > 0),
-    CHECK (prix_unitaire >= 0)
+    FOREIGN KEY (id_commande) REFERENCES commandes(id),
+    FOREIGN KEY (id_produit) REFERENCES produits(id),
+    CHECK (quantite > 0)
 );
+
